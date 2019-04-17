1dd7cc4d7d0ca333cdeb4951d1b679d20410a76c
GuaiYiHu <guaiyihu@foxmail.com>
2019-04-17 18:29:49 +0800

    ginkgo: Build thermal hidl impl and service from source
    Change-Id: I0509d993d39fa73763dc5c548c3853295a7669dd
    Signed-off-by: Ashwin R C <ashwin2001achu@gmail.com>

--- device.mk
+++ device.mk
@@ -265,6 +265,11 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
 # VNDK-SP
 PRODUCT_PACKAGES += \
     vndk_package
