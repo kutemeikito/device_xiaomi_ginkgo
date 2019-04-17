994af0a66eb2bf665e8b323308313f9c23bb4514
GuaiYiHu <guaiyihu@foxmail.com>
2019-04-17 18:26:54 +0800

    violet: device: Build libjson from source
    Change-Id: I8217069e7f99a9c9d0535c2f79bf5f065e288b3b
    Signed-off-by: Ashwin R C <ashwin2001achu@gmail.com>

--- device.mk
+++ device.mk
@@ -215,6 +215,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/permissions/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
 
+# QMI
+PRODUCT_PACKAGES += \
+    libjson
+
 # RCS
 PRODUCT_PACKAGES += \
     rcs_service_aidl \
