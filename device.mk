8fb85c4c2d58b1bc1d7067eb043a8f1d8af12f10
GuaiYiHu <guaiyihu@foxmail.com>
2019-04-17 19:26:44 +0800

    violet: device: Build health hidl service from source
    Change-Id: Iebe776011b121c4f2a10761b8737eb11c7faaf5f
    Signed-off-by: Ashwin R C <ashwin2001achu@gmail.com>

--- device.mk
+++ device.mk
@@ -217,6 +217,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libsensorndkbridge
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.0-service
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
