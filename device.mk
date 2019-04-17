6d77b37ac1581c2c7b1308edd09462ddc709ebe7
GuaiYiHu <guaiyihu@foxmail.com>
2019-04-17 18:30:36 +0800

    ginkgo: Build libtinyxml2 from source
    Change-Id: Ib6e4d50d665ed70a0c66412f0755041b9f9ed459
    Signed-off-by: Ashwin R C <ashwin2001achu@gmail.com>

--- device.mk
+++ device.mk
@@ -270,6 +270,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
+# Touchscreen
+PRODUCT_PACKAGES += \
+    libtinyxml2
+
 # VNDK-SP
 PRODUCT_PACKAGES += \
     vndk_package
