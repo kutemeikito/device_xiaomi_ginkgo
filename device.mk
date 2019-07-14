35e791e2e443aabb296a14277583ba6dde7f247b
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-07-14 18:57:17 +0200

    ginkgo: Build common Trust HAL
    Change-Id: I73a24535534a696ae42cdda3195c60aa2e50ff33

--- device.mk
+++ device.mk
@@ -116,6 +116,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
+# Trust HAL
+PRODUCT_PACKAGES += \
+    lineage.trust@1.0-service
+
 # VNDK-SP
 PRODUCT_PACKAGES += \
     vndk_package
