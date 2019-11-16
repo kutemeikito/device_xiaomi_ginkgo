03e352f31ac149fead119f87da69b3ca67a7adc0
Ashwin R C <ashwin2001achu@gmail.com>
2019-11-16 11:22:55 +0000

    ginkgo: build 32 bit version of android.hardware.camera.provider@2.4-service

--- device.mk
+++ device.mk
@@ -123,7 +123,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64 \
+    android.hardware.camera.provider@2.4-service \
     libxml2 \
     Snap
 
