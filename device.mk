ebd91b3bab85c88b785a6ea74d8680455b260c68
Ashwin R C <ashwin2001achu@gmail.com>
2019-11-18 11:48:16 +0000

    ginkgo: build android.hidl.manager

--- device.mk
+++ device.mk
@@ -220,6 +220,8 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
+    android.hidl.manager-V1.0-java \
+    android.hidl.manager@1.0-java \
     android.hidl.manager@1.0_system
 
 # IFAA manager
