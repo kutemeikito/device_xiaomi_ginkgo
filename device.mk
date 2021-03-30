94ae4ca287dabf1a4b70d15677278a58a309d934
erwinabs <erwinabubakarsidik@gmail.com>
2021-03-30 11:44:31 +0000

    ginkgo: Import Perf config from LA.UM.9.11.r1-02800-NICOBAR.0.
    Signed-off-by: erwinabs <erwinabubakarsidik@gmail.com>

--- device.mk
+++ device.mk
@@ -308,6 +308,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf \
+    $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
     $(LOCAL_PATH)/configs/perf/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 # QTI VNDK Framework Detect
