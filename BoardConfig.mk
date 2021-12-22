476cdba93d3fd6183dfa699121bc37f935a36369
Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>
2021-12-22 15:53:58 +0000

    Boardconfig: Switch to Snapdragon Clang for build
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -117,8 +117,8 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/ginkgo
 TARGET_KERNEL_CONFIG := vendor/ginkgo-perf_defconfig
 
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_ADDITIONAL_FLAGS := LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
-TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
+TARGET_KERNEL_CLANG_VERSION := sdllvm
+TARGET_KERNEL_ADDITIONAL_FLAGS := AR=llvm-ar AS=llvm-as NM=llvm-nm LD=ld.lld OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump OBJSIZE=llvm-size READELF=llvm-readelf STRIP=llvm-strip HOSTAR=llvm-ar HOSTAS=llvm-as HOSTNM=llvm-nm HOSTLD=ld.lld HOSTCFLAGS="-fuse-ld=lld"
 
 # NFC
 TARGET_USES_NQ_NFC := true
