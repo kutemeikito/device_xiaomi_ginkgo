2ea38c2ac84373a88473f5b64395818b889983aa
Edwiin kusuma jaya <kutemeikito0905@gmail.com>
2022-03-13 20:41:00 +0700

    Ancient: Switch to Redfin 05/02/2022
    Signed-off-by: Edwiin kusuma jaya <kutemeikito0905@gmail.com>

--- ancient_ginkgo.mk
+++ ancient_ginkgo.mk
@@ -43,4 +43,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.211205.017/7955197:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:12/SQ1A.220205.002/8010174:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="redfin-user 12 SQ1A.220205.002 8010174 release-keys"
