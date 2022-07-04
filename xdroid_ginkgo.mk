f1be8d0b7befb91e25ee836c8b9fb8a19f7c6a3b
Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>
2022-07-04 14:03:12 +0700

    add xdroid_ginkgo.mk
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

---
+++ xdroid_ginkgo.mk
@@ -0,0 +1,49 @@
+#
+# Copyright (C) 2020 ArrowOS
+# Copyright (C) 2020 AncientRoms
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit framework first
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit from ginkgo device
+$(call inherit-product, device/xiaomi/ginkgo/device.mk)
+
+# Inherit some common AncientRoms stuff
+XDROID_BOOT := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true.
+XDROID_MAINTAINER := Ryuzenn
+TARGET_INCLUDE_STOCK_ARCORE := true
+XDROID_UI_BLUR := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+$(call inherit-product, vendor/xdroid/config/common.mk)
+
+# Device identifier
+PRODUCT_NAME := xdroid_ginkgo
+PRODUCT_DEVICE := ginkgo
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi Note 8
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "google/redfin/redfin:12/SQ1A.220505.002/8353555:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="redfin-user 12 SQ1A.220505.002/8353555 release-keys"
