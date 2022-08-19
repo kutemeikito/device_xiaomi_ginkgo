efb38540ff8d922a0202e9351889993831b8b18c
Aoihara <aoihara@nusantararom.org>
2022-08-19 12:19:38 +0000

    ginkgo: Make shim for lib-watermarkshim.so
    Author: Aoihara <aoihara@nusantararom.org>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- device.mk
+++ device.mk
@@ -399,7 +399,8 @@ PRODUCT_PACKAGES += \
 
 # Shims
 PRODUCT_PACKAGES += \
-    lib-imsvt
+    lib-imsvt \
+    lib-watermarkshim
 
 # Soong namespace
 PRODUCT_SOONG_NAMESPACES += \
--- libshims/Android.mk
+++ libshims/Android.mk
@@ -6,4 +6,9 @@ LOCAL_SRC_FILES := lib-imsvt.cpp
 LOCAL_MODULE := lib-imsvt
 LOCAL_MODULE_TAGS := optional
 
+LOCAL_SRC_FILES := lib-watermarkshim.cpp
+LOCAL_MODULE := lib-watermarkshim
+LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
+
 include $(BUILD_SHARED_LIBRARY)
