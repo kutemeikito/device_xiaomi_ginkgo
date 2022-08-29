af6a2dce5bb0790b880762111c234c7bd8b72483
Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>
2022-08-29 11:54:43 +0000

    nuke keyhandler
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- device.mk
+++ device.mk
@@ -228,7 +228,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
 # Keyhandler
-PRODUCT_PACKAGES += \
+# PRODUCT_PACKAGES += \
     KeyHandler
 
 # Keymaster
