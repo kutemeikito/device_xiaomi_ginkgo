1de84d682967a73fd8af9a8bd380cadb6687c730
Luofan Chen <clfbbn@gmail.com>
2023-03-01 20:51:49 +0800

    ginkgo: Build lineage health HAL
    Change-Id: I1ca5f25f3a2d8734647b9cc0ff4885b0b817c6ad
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <Kutemeikito0905@gmail.com>

--- device.mk
+++ device.mk
@@ -248,6 +248,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.ginkgo
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+    
 # Media
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
