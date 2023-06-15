9859eb45763a1811f6634e59a926e589095b0175
Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>
2023-06-15 07:10:14 +0000

    ginkgo: update fingerprint to cheetah
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- ancient_ginkgo.mk
+++ ancient_ginkgo.mk
@@ -50,6 +50,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/cheetah/cheetah:13/TD1A.221105.001/9104446:user/release-keys"
+BUILD_FINGERPRINT := "google/cheetah/cheetah:13/TQ2A.230405.003.E1/9802792:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="cheetah-user 13 TD1A.221105.001/9104446 release-keys"
+    PRIVATE_BUILD_DESC="cheetah-user 13 TQ2A.230405.003.E1/9802792 release-keys"
