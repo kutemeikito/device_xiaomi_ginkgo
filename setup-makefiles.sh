aa06b475c0a9f025123a2a46b6d5e14f157ce07e
Aaron Kling <webgeek1234@gmail.com>
2023-08-29 15:58:31 -0500

    ginkgo: Add vendor soong imports to extract
    Change-Id: Ib19e69dc35af6b04519eeb695228af65f3d01d2a
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,6 +25,19 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
+function vendor_imports() {
+    cat <<EOF >>"$1"
+       "device/xiaomi/ginkgo",
+       "hardware/qcom-caf/sm8150",
+       "hardware/qcom-caf/wlan",
+       "vendor/qcom/opensource/commonsys/display",
+       "vendor/qcom/opensource/commonsys-intf/display",
+       "vendor/qcom/opensource/dataservices",
+       "vendor/qcom/opensource/data-ipa-cfg-mgr-legacy-um",
+       "vendor/qcom/opensource/display",
+EOF
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 
