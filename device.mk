54d0f9affe94d9948a2251de69580a6b1d70737d
basamaryan <basam.aryan@gmail.com>
2023-10-07 15:17:04 -0400

    ginkgo: Build protobuf vendorcompat
    Change-Id: I724e933e046cbf631c078321d55550b96ca998da
    Signed-off-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- device.mk
+++ device.mk
@@ -367,6 +367,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libjson \
     libprotobuf-cpp-full \
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat \
     librmnetctl
 
 PRODUCT_PACKAGES += \
