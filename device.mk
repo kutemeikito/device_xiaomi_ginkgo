8db89356a16c7e6ef2c6ead75733f9369af5cc12
Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>
2025-04-26 15:24:09 +0000

    ginkgo: drop lib/libhidlbase-v32.so
    error: build/soong/fsgen/Android.bp:31:1: module "aosp_ginkgo_generated_vendor_image" variant "android_common" (created by module "soong_filesystem_creator" variant "android_common"): packaging conflict at lib64/libhidlbase-v32.so:
    {lib64/libhidlbase-v32.so {{{.intermediates/prebuilts/vndk/v32/arm64/arch-arm-armv8-a/shared/vndk-sp/vendor-prebuilts_vndk_v32_arm64_arch-arm-armv8-a_shared_vndk-sp-lib-0/android_arm64_armv8-a_cortex-a73/libhidlbase-v32.so libhidlbase-v32.so} out/soong out/soong/.intermediates/prebuilts/vndk/v32/arm64/arch-arm-armv8-a/shared/vndk-sp/vendor-prebuilts_vndk_v32_arm64_arch-arm-armv8-a_shared_vndk-sp-lib-0/android_arm64_armv8-a_cortex-a73/libhidlbase-v32.so}}  false 0xc298110ea0 vendor false 0xc2d98f0fc0 {arm64 Arm64 lib64} 0xc298110fd8 vendor-prebuilts_vndk_v32_arm64_arch-arm-armv8-a_shared_vndk-sp-lib-0}
    {lib64/libhidlbase-v32.so {{{.intermediates/prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/vendor-prebuilts_vndk_v32_arm64_arch-arm64-armv8-a_shared_vndk-sp-lib64-0/android_arm64_armv8-a_cortex-a73/libhidlbase-v32.so libhidlbase-v32.so} out/soong out/soong/.intermediates/prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/vendor-prebuilts_vndk_v32_arm64_arch-arm64-armv8-a_shared_vndk-sp-lib64-0/android_arm64_armv8-a_cortex-a73/libhidlbase-v32.so}}  false 0xc2963911e8 vendor false 0xc30f9eb7c0 {arm64 Arm64 lib64} 0xc296391218 vendor-prebuilts_vndk_v32_arm64_arch-arm64-armv8-a_shared_vndk-sp-lib64-0}
    
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- device.mk
+++ device.mk
@@ -452,7 +452,6 @@ PRODUCT_COPY_FILES += \
 
 # VNDK
 PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v32/arm64/arch-arm-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhidlbase-v32.so \
     prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so \
     prebuilts/vndk/v34/arm64/arch-arm64-armv8-a/shared/vndk-core/libcrypto.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libcrypto-v34.so
 
