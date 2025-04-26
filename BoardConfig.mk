fdaa196d14fa6cfcfe82be91cb957c6ae1b41dc4
Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>
2025-04-26 13:20:56 +0000

    Revert "ginkgo: Nuke ATFWD"
    This reverts commit e4ae36ef01a7aa97202395584f8b29bc4b5df6a0.
    
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -93,6 +93,7 @@ USE_OPENGL_RENDERER := true
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml \
     vendor/aosp/config/device_framework_matrix.xml
+DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/framework_manifest.xml
 DEVICE_MANIFEST_FILE := \
      $(DEVICE_PATH)/configs/hidl/manifest.xml \
      hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest.xml
